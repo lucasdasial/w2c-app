8d88e2f803bee80e0a8e3ce87f324b382b75d60b
Lucas Alves <as.lucasalves@gmail.com>
2022-07-20 18:18:08 -0300

    home screen started, recovering data from store

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import { VStack } from "native-base";
+import { useState } from "react";
 import { Header } from "../components/Header";
+import { IWinner } from "../entities/winner";
 
 export function Home() {
   const navigation = useNavigation();
+  const [list, setList] = useState([]);
+
+  const getWinners = async () => {
+    const jsonValue = await AsyncStorage.getItem("@winners");
+
+    const win = jsonValue != null ? JSON.parse(jsonValue) : null;
+
+    console.log(win);
+  };
+
+  getWinners();
 
   return (
     <VStack flex={1} alignItems="center" bg="rose.900">
